187d8c5dae63b8093f715a6641550b095d0a44e9
lalana-29 <alexandra.l.davison@gmail.com>
2026-05-01 21:31:43 -0400

    changed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k2a#3%@z86e9cy7r2k2bn-+_h2u+!*+tq#q_!x!wo*h(q46_56
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cs348project-production-55b2.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
